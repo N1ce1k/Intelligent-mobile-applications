12d87228ee02da357cf9eccc3bc6509d90ceee9f
N1ce1k <lsichinskiy@gmail.com>
2025-03-14 23:37:32 +0300

    Initial commit

---
+++ buttonclicker/src/main/java/com/mirea/sichinskiyle/buttonclicker/MainActivity.java
@@ -0,0 +1,81 @@
+package com.mirea.sichinskiyle.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox cbYesIAm;
+    private boolean flag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        cbYesIAm = findViewById(R.id.checkBox);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = true;
+                textViewStudent.setText("Мой номер по списку № 23");
+                cbYesIAm.setText("ВКЛ");
+                cbYesIAm.setChecked(flag);
+            }
+        };
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+
+        View.OnClickListener oclbtnItIsNotMe = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = false;
+                textViewStudent.setText("Это не я сделал");
+                cbYesIAm.setText("ВЫКЛ");
+                cbYesIAm.setChecked(flag);
+            }
+        };
+        btnItIsNotMe.setOnClickListener(oclbtnItIsNotMe);
+
+
+
+        View.OnClickListener oclCheckBox = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = !flag;
+                if (flag == true)
+                {
+                    cbYesIAm.setText("ВКЛ");
+                }
+                else
+                {
+                    cbYesIAm.setText("ВЫКЛ");
+                }
+                cbYesIAm.setChecked(flag);
+            }
+        };
+        cbYesIAm.setOnClickListener(oclCheckBox);
+
+    }
+    public void onMyButtonClick(View view)
+    {
+        flag = false;
+        Toast.makeText(this, "Ещё способ!", Toast.LENGTH_SHORT).show();
+        cbYesIAm.setText("ВЫКЛ");
+        cbYesIAm.setChecked(flag);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+Отчёт по практике № 1: 
+В ходе практической работы была настроена среда разработки – Android Studio. В ней было создан проект с шаблоном «Empty Views Activity», со следующими параметрами:
+•	Name – «Lesson1». 
+•	Package name – «ru.mirea.SichinskiyLE.lesson1». 
+•	Minimum SDK – API 26. 
+Далее был создан эмулятор android устройства: «Pixel 9». 
+
+Добавление элемента TextView на экран и изменение отображаемого текста:
+![img.png](screenshoots%2Fimg.png)
+
+экран редактирования разметки:
+![img_1.png](screenshoots%2Fimg_1.png)
+
+Добавлен элемент TextView на экран и изменен отображаемый текст:
+![img_2.png](screenshoots%2Fimg_2.png)
+
+Создание различных типов Layout:
+![img_3.png](screenshoots%2Fimg_3.png)
+
+Экран TableLayout:
+![img_4.png](screenshoots%2Fimg_4.png)
+
+
